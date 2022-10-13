269973cf9262d69a4dd17cfd9490af13cbe3e4cd
Sachin Mohan <sachinkm308@gmail.com>
2022-10-13 13:21:48 +0200

    initial setup

---
+++ src/game/Card.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Card {
+
+    public final String suit;
+    public final String value;
+
+    String[] VALUE = { null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    String[] Suits = { "Clubs", "Diamonds", "Hearts", "Spades"};
+    //public enum CardSuit { C, D, H, S };
+    //public enum CardValue { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    public Card(String suit, String value){
+        this.suit = suit;
+        this.value = value;
+    }
+}
---
+++ src/game/PokerMain.java
@@ -0,0 +1,10 @@
+package game;
+
+public class PokerMain {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello world!");
+        Card twoofdiamonds = new Card("Clubs", "2");
+        System.out.println("three of diamonds" +threeofdiamonds);
+    }
+}
