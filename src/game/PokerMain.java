4d9a7f5b46fa1ea5902e8bbea23d67a23d64e33c
Sachin Mohan <sachinkm308@gmail.com>
2022-10-14 14:15:18 +0200

    Added 5 cards to both hands.

--- src/game/PokerMain.java
+++ src/game/PokerMain.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 public class PokerMain {
 
-    private static final List<Card> HAND1 = List.of(new Card("Clubs", "2"), new Card("Spades", "2"));
-    private static final List<Card> HAND2 = List.of(new Card("Clubs", "2"), new Card("Spades", "3"));
-    private static int[] rankCompare = { 11, 11}; // Intializing to a higher value to do the comparison
+    private static final List<Card> HAND1 = List.of(new Card("Clubs", "2"), new Card("Spades", "2"),new Card("Clubs", "3"), new Card("Diamonds", "K"), new Card("Spades", "Q"));
+    //String s = check.isAPair(HAND1););
+    private static final List<Card> HAND2 = List.of(new Card("Clubs", "2"), new Card("Spades", "3"),new Card("Clubs", "10"), new Card("Diamonds", "K"), new Card("Spades", "Q"));
+    //String s = check.isAPair(HAND1););
+    private static final int[] rankCompare = { 11, 11}; // Intializing to a higher value to do the comparison
     public static void main(String[] args) {
         testisAPair(); // to test a pair functionality for the 2 hands
     }
